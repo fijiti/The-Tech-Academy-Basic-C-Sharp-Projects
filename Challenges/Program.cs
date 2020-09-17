ff356a4369e8955bb2f6363e0fe9974a371e7812
fijiti <fijiti@users.noreply.github.com>
2020-09-17 16:10:10 -0700

    Add PackageExpress Project

--- Challenges/Program.cs
+++ Challenges/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
-
+using System.Globalization;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.WindowsRuntime;
 
 namespace Challenges
 {
@@ -15,6 +17,43 @@ namespace Challenges
             string aName = "John";
             int aNum = 7;
             Console.WriteLine(aName + aNum);
+
+            // Page 165
+            string name = "jim";
+            if (name == "jim"){
+                Console.WriteLine("Hi Jim!");
+            } else if (name == "joe") {
+                Console.WriteLine("Hi Joe!");
+            } else {
+                Console.WriteLine("I don't know who you are.");
+            }
+
+            // Page 167
+            name = "sam";
+            if (name != "jim") {
+                Console.WriteLine("Your not Jim!");
+            } else {
+                Console.WriteLine("Hi Jim!");
+            }
+
+            // Page 167
+            int num1 = 10;
+            int num2 = 12;
+            if (num1 > num2 ) {
+                Console.WriteLine("First number is greater than second number.");
+            } else if (num1 < num2) {
+                Console.WriteLine("Second number is greater than first number.");
+            } else {
+            
+                Console.WriteLine("First and Second number are equal.");
+            }
+
+            // Page 175
+            string result = num1 > num2 ? "num1 is greater than num2" : "num2 is greater than num1";
+            Console.WriteLine(result);
+
+            Console.ReadLine();
+
 
             
         }
---
+++ PackageExpress/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace PackageExpress
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Package Express");
+
+            Console.WriteLine("Please enter the package weight:");
+            int packageWeight = int.Parse(Console.ReadLine());
+            if (packageWeight > 50)
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+            }
+            else
+            {
+
+                Console.WriteLine("Please enter the package width:");
+                int packageWidth = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Please enter the package height:");
+                int packageHeight = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Please enter the package length:");
+                int packageLength = int.Parse(Console.ReadLine());
+
+                int demensions = packageWidth + packageHeight + packageLength;
+                if (demensions > 50)
+                {
+                    Console.WriteLine("Package too big to be shipped via Package Express.");
+                    
+                } else
+                {
+                    int quote = ((packageLength * packageHeight * packageWidth) * packageWeight) / 100;
+                    decimal dollarValue = Convert.ToDecimal(quote);
+                    Console.WriteLine("Your estimated total for shipping this package is: " + dollarValue.ToString("C2"));
+                    Console.WriteLine("Thank you!");
+                    
+                }
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
