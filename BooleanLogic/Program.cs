39f90f5b6a44aa0272b99e9d1679deb9d82e216c
fijiti <fijiti@users.noreply.github.com>
2020-09-17 13:59:07 -0700

    Add Basic Aproval Program project

---
+++ BooleanLogic/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+
+namespace BooleanLogic
+{
+    class Program
+    {
+        static void Main()
+        {
+            //Console.WriteLine(true && false); // false
+            //Console.WriteLine(true && true); // true
+            //Console.WriteLine(false && false); // false
+
+            //Console.WriteLine(true || true); // true 
+            //Console.WriteLine(true || false); // true
+            //Console.WriteLine(false || false); // false
+
+            //Console.WriteLine(true == true); // true
+            //Console.WriteLine(true == false); // false 
+            //Console.WriteLine(false == false); // true
+
+            //Console.WriteLine(true != true); // false
+            //Console.WriteLine(true != false); // true 
+            //Console.WriteLine(false != false); // false 
+
+            // XOR operator below.  It is true when it is exclusively true because of OR not AND.
+            //Console.WriteLine(true ^ true); // false 
+            //Console.WriteLine(true ^ false); // true
+            //Console.WriteLine(false ^ false); // false 
+
+            // Multiple Ands and ORs allowed. 
+            Console.WriteLine(true && true && true || false); // true 
+            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ InsuranceApproval/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+
+namespace InsuranceApproval
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Basic Approval Program for Car Insurance");
+
+            Console.WriteLine("What is your age?");
+            int age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Have you ever had a DUI (true or false)?");
+            bool hadDUI = bool.Parse(Console.ReadLine());
+
+            Console.WriteLine("How many speeding tickes do you have?");
+            int numberOfTickets = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Qualafied?");
+            bool qualified;
+            if (age > 15 && hadDUI == false && numberOfTickets <= 3 )
+            {
+                qualified = true;
+            } else
+            {
+                qualified = false;
+            }
+            Console.WriteLine(qualified);
+            
+            Console.ReadLine();
+           
+
+        }
+    }
+}
