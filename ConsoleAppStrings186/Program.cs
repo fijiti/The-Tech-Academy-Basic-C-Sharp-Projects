91b76eef372e9bc1d61c57576da46e4bd488b4f6
fijiti <fijiti@users.noreply.github.com>
2020-09-18 12:46:05 -0700

    Add ConsoleAppStrings186 and String practice

---
+++ ConsoleAppStrings186/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text;
+
+namespace ConsoleAppStrings186
+{
+    class Program
+    {
+        static void Main()
+        {
+            // 1) Concatenates three strings.
+            Console.WriteLine("Let's concatenate three strings:");
+            Console.WriteLine("Enter one string:");
+            string string1 = Console.ReadLine();
+            Console.WriteLine("Enter another string:");
+            string string2 = Console.ReadLine();
+            Console.WriteLine("Enter one more string");
+            string string3 = Console.ReadLine();
+
+            string allStrings = string1 + string2 + string3;
+            Console.WriteLine("Below are your three strings concatenated:");
+            Console.WriteLine(allStrings);
+
+            // 2) Converts a string to uppercase
+            Console.WriteLine("\nInput a string and we will convert it to upper case:");
+            string aString = Console.ReadLine();
+            Console.WriteLine(aString.ToUpper());
+
+            // 3) Creates a StringBuilder and builds a paragraph of text one sentence at a time. 
+            StringBuilder sb = new StringBuilder();
+
+            Console.WriteLine("\nLet's write paragraph 1 sentence at a time to 3 sentences:");
+            int i = 0;
+            do
+            {
+                Console.WriteLine("Enter sentence:");
+                sb.Append(Console.ReadLine()+" ");
+                i++;
+
+            } while (i < 3);
+            Console.WriteLine("Here is your paragraph:");
+            Console.WriteLine(sb.ToString());
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ StringFunctions/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+
+namespace StringFunctions
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Step 184
+            string name = "Jesse";
+
+            string quote = "The man said, \"Hello\" Jessie. \nHello on a new line. \n\tHello on a tab.";
+            Console.WriteLine($"quote: {quote}");
+
+            string fileName = @"C:\Users\Jesse";
+            Console.WriteLine($"fileName: {fileName}");
+
+            bool trueOrFalse = name.Contains("s");
+            Console.WriteLine($"Jesse Contains s: {trueOrFalse}");
+
+            trueOrFalse = name.EndsWith("z");
+            Console.WriteLine($"Jesse EndsWith z: {trueOrFalse}");
+
+            int length = name.Length;
+            Console.WriteLine($"Jesse name length: {name.Length}");
+
+            Console.WriteLine($"Jesse name.ToUpper(): {name.ToUpper()}");
+            Console.WriteLine($"Jesse name.ToLower(): {name.ToLower()}");
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("My name is Jesse");
+            Console.WriteLine($"StringBuilder sb.Append(): {sb}");
+
+            Console.ReadLine();
+        }
+    }
+}
