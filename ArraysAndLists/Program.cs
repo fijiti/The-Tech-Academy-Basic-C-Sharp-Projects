d7bfc9b69c08044150cfbd81ba1e26eccefd3589
fijiti <fijiti@users.noreply.github.com>
2020-09-18 15:24:27 -0700

    Add ConsoleAppArraysAndLists191 and ArraysAndLists training practice

---
+++ ArraysAndLists/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ArraysAndLists
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Page 187 Array Practice
+            Console.WriteLine("Testing values for different ways of initializing Arrays in C#:");
+            int[] numArray = new int[5];
+            numArray[0] = 5;
+            numArray[1] = 2;
+            numArray[2] = 10;
+            numArray[3] = 200;
+            numArray[4] = 5000;
+            Console.WriteLine($"numArray[3] 200?: { numArray[3]}");
+
+            int[] numArray1 = new int[] { 5, 2, 10, 200, 5000, 600, 2300 };
+            Console.WriteLine($"numArray1[3] 200?: { numArray1[3]}");
+
+            int[] numArray2 = { 5, 2, 10, 200, 5000, 600, 2300 };
+            Console.WriteLine($"numArray2[3] 200?: {numArray2[3]}");
+
+            Console.WriteLine("\nChanging data in array:");
+            Console.WriteLine($"numArray2[5] before change: {numArray2[5]}");
+            numArray2[5] = 650;
+            Console.WriteLine($"numArray2[5] after change: {numArray2[5]}");
+
+            //Now working with lists
+            Console.WriteLine("\nNow using Lists:");
+
+            List<int> intList = new List<int>();
+            intList.Add(4);
+            intList.Add(10);
+
+
+            Console.WriteLine($"intList[0] 4?: {intList[0]}");
+            Console.WriteLine($"intList[1] 10?: {intList[1]}");
+            intList.Remove(10);
+            intList.Add(33);
+            Console.WriteLine($"intList[1] after intList.Remove(10) and intList.Add(33): { intList[1]}");
+
+            Console.WriteLine("\nList of strings:");
+            List<string> stringList = new List<string>();
+            stringList.Add("Hello!");
+            stringList.Add("Hello Again!");
+            Console.WriteLine($"stringList[0]: {stringList[0]}\nstringList[1]: {stringList[1]}");
+
+           
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleAppArraysAndLists191/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace ConsoleAppArraysAndLists191
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Page 191
+            // Array of strings 
+            string[] names = { "John", "Jim", "Joe", "Mary", "Elmer" };
+            Console.WriteLine("Help me test out the Indexes of my String Array of Names:");
+            Console.WriteLine("Choose an index number:");
+            int number = int.Parse(Console.ReadLine());
+            int i = 0;
+            do
+            {
+                if (number >= 0 && number < names.Length)
+                {
+                    Console.WriteLine($"Name: {names[number]}");
+                    if (i == 3) { break; }
+                    else
+                    {
+                        Console.WriteLine("Choose Another:");
+                        number = int.Parse(Console.ReadLine());
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Not in Array.");
+                    if (i == 3) { break; }
+                    else
+                    {
+                        Console.WriteLine("Choose Another:");
+                        number = int.Parse(Console.ReadLine());
+                    }
+                }
+                i++;
+            } while (i < 4);
+            Console.WriteLine("Thanks!");
+
+            // Array of integers 
+            int[] numbers = { 4, 5, 33, 55, 344, 45, 54 };
+            Console.WriteLine("\nHelp me test out the Indexes of my Integer Array of Numbers:");
+            Console.WriteLine("Choose an index number:");
+            int num1 = int.Parse(Console.ReadLine());
+            int y = 0;
+
+            do
+            {
+                if (num1 >= 0 && num1 < numbers.Length)
+                {
+                    Console.WriteLine($"Number: {numbers[num1]}");
+                    if (y == 3) { break; }
+                    else
+                    {
+                        Console.WriteLine("Choose Another:");
+                        num1 = int.Parse(Console.ReadLine());
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Not in Array.");
+                    if (y == 3) { break; }
+                    else
+                    {
+                        Console.WriteLine("Choose Another:");
+                        num1 = int.Parse(Console.ReadLine());
+                    }
+                }
+                y++;
+            } while (y < 4);
+            Console.WriteLine("Thanks!");
+
+            // List of strings
+            List<string> animals = new List<string>(){"Bear", "Lion", "Tiger", "Giraffe", "Moose", "Wolf", "Fox"};
+            Console.WriteLine("\nHelp me test out the Indexes of my List of Animals:");
+            Console.WriteLine("Choose an index number:");
+            int num2 = int.Parse(Console.ReadLine());
+            int z = 0;
+            do
+            {
+                if (num2 >= 0 && num2 < animals.Count)
+                {
+                    Console.WriteLine($"Animal: {animals[num2]}");
+                    if (z == 3) { break; }
+                    else
+                    {
+                        Console.WriteLine("Choose Another:");
+                        num2 = int.Parse(Console.ReadLine());
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Not in Array.");
+                    if (z == 3) { break; }
+                    else
+                    {
+                        Console.WriteLine("Choose Another:");
+                        num2 = int.Parse(Console.ReadLine());
+                    }
+                }
+                z++;
+            } while (z < 4);
+            Console.WriteLine("Thanks!");
+
+
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
