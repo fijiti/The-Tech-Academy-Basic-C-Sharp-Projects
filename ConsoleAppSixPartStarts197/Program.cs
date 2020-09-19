5e1b246d864527023d588fd5d30a6309532dcfeb
fijiti <fijiti@users.noreply.github.com>
2020-09-18 19:52:11 -0700

    Update for ConsoleAppSixPartStarts197 {

---
+++ ConsoleAppSixPartStarts197/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace ConsoleAppSixPartStarts197
+{
+    class Program
+    {
+        static void Main()
+        {
+        //    // Array that has user input strings 
+        //    string[] words = new string[4];
+        //    Console.WriteLine("Part One:");
+        //    Console.WriteLine("Please enter a word:");
+        //    string word;
+
+        //    for (int i = 0; i < words.Length; i++)
+        //    {
+        //        Console.WriteLine("Enter another word:");
+        //        word = Console.ReadLine();
+        //        words[i] = word;
+        //    }
+        //    Console.WriteLine("Here are the words you entered:");
+        //    foreach (string item in words)
+        //    {
+        //        Console.WriteLine(item);
+
+        //    }
+
+        //    Console.WriteLine("\nPart Two:");
+        //    Console.WriteLine("Below is Finite loop. Committed out infinite loop:");
+        //    // Infinate loop and then fixed
+        //    //bool isTrue = true;
+        //    //while (isTrue)
+        //    //{
+        //    //    Console.WriteLine("Hello");
+        //    //}
+        //    bool isTrue = false;
+        //    int x = 0;
+        //    while(!isTrue)
+        //    {
+        //        if(x < 4)
+        //        {
+        //            Console.WriteLine("Continuing Loop!");
+        //        } else
+        //        {
+        //            isTrue = true;
+        //            Console.WriteLine("Loop ending now!");
+        //        }
+        //       x++;
+        //    }
+
+            // Part 3
+            //Console.WriteLine("\nPart 3:");
+            //Console.WriteLine("Loop will continue while number is < 4:");
+            //for (int i = 0; i < 6; i++)
+            //{
+            //    if(i < 4)
+            //    {
+            //        Console.WriteLine($"Number is: {i}");
+
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Number is no longer < 4.");
+            //        break;
+            //    }
+
+            //}
+
+            //Console.WriteLine("\nLoop contineus while number <= 4:");
+            //for (int i = 0; i < 6; i++)
+            //{
+            //    if(i <= 4)
+            //    {
+            //        Console.WriteLine($"Number is: {i}");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Number is no longer <= 4.");
+            //        break;
+            //    }
+            //}
+
+
+            // Part 4:
+            // 
+
+
+            Console.ReadLine();
+        }
+    }
+}
