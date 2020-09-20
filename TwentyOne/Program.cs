f17ac6cd5a1712a83491f371b6cebd3b7dec6eac
fijiti <fijiti@users.noreply.github.com>
2020-09-19 18:27:04 -0700

    Add TwentOne project.  Changed overall folder to Solution

---
+++ TwentyOne/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TwentyOne
+{
+    class Program
+    {
+        static void Main()
+        {
+            Deck deck = new Deck();
+            foreach (var card in deck.Cards)
+            {
+                Console.WriteLine($"{card.Face} of {card.Suit}");
+            }
+
+            Console.WriteLine($"Card Count: {deck.Cards.Count}");
+            Console.ReadLine();
+        }
+    }
+}
