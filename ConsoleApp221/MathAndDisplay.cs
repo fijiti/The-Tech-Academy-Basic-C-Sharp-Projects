74c1cf4ede1a6ce013ffc227e9f929a143376f0e
fijiti <fijiti@users.noreply.github.com>
2020-09-21 12:44:35 -0700

    Add ConsoleApp221

---
+++ ConsoleApp221/MathAndDisplay.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp221
+{
+    class MathAndDisplay
+    {
+        public void SquareOneShowOne(int number1, int number2)
+        {
+            int result = number1 * number1;
+            Console.WriteLine($"{number1} Squared is: {result}");
+            Console.WriteLine($"Second number provided {number2}");
+        }
+    }
+}
---
+++ ConsoleApp221/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp221
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathAndDisplay mathAndShow = new MathAndDisplay();
+            mathAndShow.SquareOneShowOne(55, 44);
+            mathAndShow.SquareOneShowOne(number1: 27, number2: 777);
+            Console.ReadLine();
+        }
+    }
+}
