d9991196d92315d68cbdd19a19480959b928494f
fijiti <fijiti@users.noreply.github.com>
2020-09-21 12:20:54 -0700

    Add OptionalParameters220

---
+++ OptionalParamaters220/MathOptions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalParameters220
+{
+    class MathOptions
+    {
+        public int AddOneOrTwo(int num1, int num2 = 0)
+        {
+            return num1 + num2;
+        }
+
+    }
+}
---
+++ OptionalParamaters220/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalParameters220
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathOptions addOneOrTwo = new MathOptions();
+            Console.WriteLine("Testing optional parameter:");
+            int result = 0;
+            Console.WriteLine("Enter first number:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second number (Optional):");
+            string number = Console.ReadLine();
+            if (string.IsNullOrEmpty(number))
+            {
+                result = addOneOrTwo.AddOneOrTwo(num1);
+                Console.WriteLine($"{num1} + 0 = {result}");
+            }
+            else
+            {
+                int num2 = Convert.ToInt32(number);
+                result = addOneOrTwo.AddOneOrTwo(num1, num2);
+                Console.WriteLine($"{num1} + {num2} = {result}"); 
+            }
+            Console.WriteLine("Thanks!");
+            Console.ReadLine();
+
+        }
+    }
+}
