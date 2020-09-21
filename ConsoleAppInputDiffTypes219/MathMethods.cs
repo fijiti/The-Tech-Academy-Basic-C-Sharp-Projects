cae480f970d044321a6d5ed33954adb354e5555e
fijiti <fijiti@users.noreply.github.com>
2020-09-20 20:17:29 -0700

    Add ConsoleAppInputDiffTypes219

---
+++ ConsoleAppInputDiffTypes219/MathMethods.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppInputDiffTypes219
+{
+    class MathMethods
+    {
+       public int MathMethod(int number)
+        {
+            return number * number;
+        }
+
+        public int MathMethod(decimal number)
+        {
+            int numberRevised = Convert.ToInt32(number);
+            return numberRevised + numberRevised;
+            
+        }
+
+        public int MathMethod(string number)
+        {
+            int numberRevised = Convert.ToInt32(number);
+            return numberRevised - numberRevised;
+        }
+    }
+}
---
+++ ConsoleAppInputDiffTypes219/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppInputDiffTypes219
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MathMethods answers = new MathMethods();
+            Console.WriteLine($"Passed 12 to MathMethod(int number) 'Returns 12 * 12 Result: {answers.MathMethod(12)}");
+            Console.WriteLine($"Passed 12.44m MathMethod(decimnal number) Returns 12 + 12 Result: { answers.MathMethod(12.44m)}");
+            Console.WriteLine($"Passed \"8\" to MathMethod(string number) Returns 8 - 8 Result: {answers.MathMethod("8")}");
+            Console.WriteLine("All overloaded methods returned an int result.");
+            Console.ReadLine();
+        }
+    }
+}
