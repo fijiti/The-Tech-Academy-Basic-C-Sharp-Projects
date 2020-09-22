af3ab57cd4b059f99337fc65ac0fdda67e39fc58
fijiti <fijiti@users.noreply.github.com>
2020-09-22 11:30:58 -0700

    Corrections made on ConsoleApp245

--- ConsoleApp245/Program.cs
+++ ConsoleApp245/Program.cs
@@ -12,24 +12,29 @@ namespace ConsoleApp245
         {
             Employee employee1 = new Employee();
             Employee employee2 = new Employee();
+            Employee employee3 = new Employee();
             employee1.FirstName = "Joe";
             employee1.LastName = "Smith";
             employee1.Id = 2;
             employee2.FirstName = "Jim";
             employee2.LastName = "Smith";
             employee2.Id = 3;
+            employee3.FirstName = "Joe";
+            employee3.LastName = "Smith";
+            employee3.Id = 2;
+            
 
             Console.WriteLine("Employee Id's different:");
             Console.WriteLine("Calling overloaded ==:");
-            Console.WriteLine(employee1.Id == employee2.Id);
+            Console.WriteLine(employee1 == employee2);
             Console.WriteLine("Calling overloaded !=:");
-            Console.WriteLine(employee1.Id != employee2.Id);
+            Console.WriteLine(employee1 != employee2);
             
-            Console.WriteLine("\nTrying Id's same employee:");
+            Console.WriteLine("\nEmployee Id's same:");
             Console.WriteLine("Calling overloaded ==:");
-            Console.WriteLine(employee2.Id == employee2.Id);
+            Console.WriteLine(employee2 == employee3);
             Console.WriteLine("Calling overloaded !=:");
-            Console.WriteLine(employee2.Id != employee2.Id);
+            Console.WriteLine(employee2 != employee3);
 
             Console.ReadLine();
 
