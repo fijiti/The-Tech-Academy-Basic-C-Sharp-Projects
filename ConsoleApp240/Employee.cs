0ba9c5bb5c78f3b3f11e6d57728e27d2757d118e
fijiti <fijiti@users.noreply.github.com>
2020-09-21 17:45:48 -0700

    Add ConsoleApp240 Update TwentyOne using interface

---
+++ ConsoleApp240/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp240
+{
+    public class Employee : Person, IQuittable
+    {
+        public override void SayName()
+        {
+            base.SayName();
+        }
+        public void Quit()
+        {
+            Console.WriteLine("You can quit if you want. But we don't want you too!");
+        }
+
+    }
+}
--- TwentyOne/TwentyOneGame.cs
+++ TwentyOne/TwentyOneGame.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace TwentyOne
 {
-    public class TwentyOneGame : Game
+    public class TwentyOneGame : Game, IWalkAway
     {
         public override void Play()
         {
@@ -19,6 +19,11 @@ namespace TwentyOne
             Console.WriteLine("21 Players:");
             // This is same method as in parent 
             base.ListPlayers();
+        }
+
+        public void WalkAway(Player player)
+        {
+            throw new NotImplementedException();
         }
     }
 }
