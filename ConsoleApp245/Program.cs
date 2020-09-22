27b8adc1f35ab0e5b779cd7ebfe9d21b1ae7ab7f
fijiti <fijiti@users.noreply.github.com>
2020-09-22 11:16:32 -0700

    Update ConsoleApp245

--- ConsoleApp245/Program.cs
+++ ConsoleApp245/Program.cs
@@ -10,6 +10,29 @@ namespace ConsoleApp245
     {
         static void Main(string[] args)
         {
+            Employee employee1 = new Employee();
+            Employee employee2 = new Employee();
+            employee1.FirstName = "Joe";
+            employee1.LastName = "Smith";
+            employee1.Id = 2;
+            employee2.FirstName = "Jim";
+            employee2.LastName = "Smith";
+            employee2.Id = 3;
+
+            Console.WriteLine("Employee Id's different:");
+            Console.WriteLine("Calling overloaded ==:");
+            Console.WriteLine(employee1.Id == employee2.Id);
+            Console.WriteLine("Calling overloaded !=:");
+            Console.WriteLine(employee1.Id != employee2.Id);
+            
+            Console.WriteLine("\nTrying Id's same employee:");
+            Console.WriteLine("Calling overloaded ==:");
+            Console.WriteLine(employee2.Id == employee2.Id);
+            Console.WriteLine("Calling overloaded !=:");
+            Console.WriteLine(employee2.Id != employee2.Id);
+
+            Console.ReadLine();
+
         }
     }
 }
