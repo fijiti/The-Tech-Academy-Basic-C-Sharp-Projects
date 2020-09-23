e63db6ca42dbad661e4234035058eec19bc333ef
fijiti <fijiti@users.noreply.github.com>
2020-09-23 14:51:59 -0700

    Add ConsoleApp254

---
+++ ConsoleApp254/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp254
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isDayOfWeek = false;
+           
+            try
+            {
+                Console.WriteLine("Please enter a day of the week capitalized:");
+                string day = Console.ReadLine();
+                DaysOfWeek aDay = (DaysOfWeek)Enum.Parse(typeof(DaysOfWeek), day, true);
+                Console.WriteLine(day);
+               
+ 
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("That is not one of the days of the week.");
+            }
+            Console.ReadLine();
+    
+        }
+
+        public enum DaysOfWeek
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday, 
+            Saturday,
+            Sunday
+        }
+
+
+    }
+}
