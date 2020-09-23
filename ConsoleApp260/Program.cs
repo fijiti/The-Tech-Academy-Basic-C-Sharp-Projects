540aaee950fad7f28b32ee4a12643606a3f2ea65
fijiti <fijiti@users.noreply.github.com>
2020-09-23 15:33:31 -0700

    Add ConsoleApp260

---
+++ ConsoleApp260/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace ConsoleApp260
+{
+    class Program
+    {
+        static void Main()
+        {
+            Number number = new Number();
+            number.Amount = 14.57m;
+            Console.WriteLine($"The Amount assigned to number is {number.Amount}");
+            Console.ReadLine();
+        }
+    }
+}
